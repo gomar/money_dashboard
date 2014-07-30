35ecf8826c85ce5cc4b71f93341bce33ea8697f4
Adrien Gomar <adrien.gomar@gmail.com>
2014-07-30 23:18:26 +0100

    select of categories is made using bootstrap select

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import Form
-from wtforms import TextField, DateField
+from wtforms import TextField, DateField, SelectField
 
 
 class AddTransactionForm(Form):
     date = DateField('date')
+    amount = TextField('amount')
     description = TextField('description')
+    category = SelectField('category', choices=[(None, ''),
+    											('household', 'Household'), 
+    	                                        ('day2day', 'Day to day'),
+    	                                        ('extras', 'Extra activities')])
+    note = TextField('note')
