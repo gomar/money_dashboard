c1954829b23a1649022ff0433719e32a8f806e69
Adrien Gomar <adrien.gomar@gmail.com>
2014-07-31 23:26:36 +0100

    adding transactions work

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,14 @@
 from flask_wtf import Form
 from wtforms import TextField, DateField, SelectField
+from wtforms.validators import Required
+
 
 
 class AddTransactionForm(Form):
-    date = DateField('date')
-    amount = TextField('amount')
-    description = TextField('description')
-    category = SelectField('category', choices=[(None, ''),
+    date = DateField('date', format='%d/%m/%Y', validators=[Required()])
+    amount = TextField('amount', validators=[Required()])
+    description = TextField('description', validators=[Required()])
+    category = SelectField('category', choices=[('', '--'),
     											('household', 'Household'), 
     	                                        ('day2day', 'Day to day'),
     	                                        ('extras', 'Extra activities')])
--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 import os
-from flask import render_template, Flask
+from flask import render_template, Flask, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
 import pandas as pd
 import forms
-from app import app, db
+from app import app, db, models
 
 
 @app.route('/')
@@ -11,15 +11,24 @@ def index():
     data = pd.read_sql_table('transaction', db.engine)
     del data['id'], data['note']
 
-    data = data.sort(['date'])
-    data['balance'] = data['amount'].cumsum()
+    data = data.sort(['date'], ascending=False)
+    data['balance'] = data['amount'][::-1].cumsum()[::-1]
 
     data = data.to_html(classes=['table table-hover table-bordered'], index=False, escape=False)
     return render_template('index.html', data=data)
 
 
-@app.route('/add_transaction')
+@app.route('/add_transaction', methods=['GET', 'POST'])
 def add_transaction():
     form = forms.AddTransactionForm()
+    if form.validate_on_submit():
+    	u = models.Transaction(date=form.date.data,
+    		                   amount=form.amount.data,
+    		                   description=form.description.data,
+    		                   category=form.category.data,
+    		                   note=form.note.data)
+    	db.session.add(u)
+    	db.session.commit()
+    	return redirect('/')
     return render_template('add_transaction.html', form=form)
 
