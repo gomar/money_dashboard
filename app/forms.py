05ec66c80afe7f9324e0948ac01b40e3cbe269d4
Adrien Gomar <adrien.gomar@gmail.com>
2014-08-03 09:58:08 +0100

    wtform deprecated

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import Form
 from wtforms import TextField, DateField, SelectField
-from wtforms.validators import Required, length
+from wtforms.validators import DataRequired, length
 from unidecode import unidecode
 
 
 class AddTransactionForm(Form):
-    date = DateField('date', format='%d/%m/%Y', validators=[Required()])
-    amount = TextField('amount', validators=[Required()])
-    description = TextField('description', validators=[Required(), length(max=20)])
-    category = SelectField('category', validators=[Required()])
+    date = DateField('date', format='%d/%m/%Y', validators=[DataRequired()])
+    amount = TextField('amount', validators=[DataRequired()])
+    description = TextField('description', validators=[DataRequired(), length(max=20)])
+    category = SelectField('category', validators=[DataRequired()])
     note = TextField('note')
