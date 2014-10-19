9571bbb55a9936d524f67a91f29b200f0cf9afb2
Adrien Gomar <adrien.gomar@gmail.com>
2014-10-19 17:27:05 +0100

    to not duplicate tabs

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
 	import webbrowser, threading
 	url = "http://127.0.0.1:5000"
 
-	threading.Timer(1.25, lambda:webbrowser.open_new(url)).start()
+	threading.Timer(1.25, lambda:webbrowser.open(url)).start()
 	app.run(debug=True)
