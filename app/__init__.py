c54790bbd1da34e787e8aa560e6c930b0616ed04
Adrien Gomar <adrien.gomar@gmail.com>
2014-10-19 15:17:05 +0100

    Commqnd not used

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.script import Manager
-from flask.ext.migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config.from_object('app.config')
