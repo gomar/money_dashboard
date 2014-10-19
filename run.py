884e1268286293d2bd49e935ed6874e8ca44514f
Adrien Gomar <adrien.gomar@gmail.com>
2014-10-19 17:14:37 +0100

    launching run open woney in the webbrowser

--- run.py
+++ run.py
@@ -3,4 +3,8 @@ from app import app
 
 
 if __name__ == "__main__":
-  app.run(debug=True)
+	import webbrowser, threading
+	url = "http://127.0.0.1:5000"
+
+	threading.Timer(1.25, lambda:webbrowser.open_new(url)).start()
+	app.run(debug=True)
